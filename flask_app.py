c2afdec512145704259470800fc96772f328c661
Marnix Oppeneer <marnix_oppeneer@hotmail.com>
2024-11-12 07:58:29 +0100

    test

--- flask_app.py
+++ flask_app.py
@@ -6,5 +6,14 @@ app=Flask(__name__)
 def home():
     return "Welkom op mijn eerste Falsk-website"
 
+
+@app.route('/prijzen')
+def prijzen():
+    return "Binnenkort verschijnen hier onze actuele prijzen"
+
+@app.route('/recepten')
+def recepten():
+    return "Binnenkort verschijnen hier enkele recepten"
+
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
